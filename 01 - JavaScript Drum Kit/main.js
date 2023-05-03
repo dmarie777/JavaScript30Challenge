5e98f24639d25068e50a42017f917925c8cea801
dmarie777 <deimarie777@gmail.com>
2023-05-02 23:51:29 -0500

    all interactions finished in main.js and all styles added in styleMySolutions

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,3 +1,20 @@
-document.addEventListener('keydown', (e) =>{
-    console.log(e.keyCode)
-})
+
+ function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key =document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0; //rewind to the start
+    audio.play();
+    key.classList.add('playing')   
+ }
+//transition event: it checks when the transition happen
+function removeTransition(e) {
+    if(e.propertyName !=="transform") return 
+    this.classList.remove('playing')
+}
+const keys= document.querySelectorAll('.key')
+
+
+keys.forEach(div => div.addEventListener('transitionend', removeTransition));
+document.addEventListener('keydown',playSound )
+
